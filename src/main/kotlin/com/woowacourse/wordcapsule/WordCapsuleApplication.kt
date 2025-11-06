f7594dac336639a36919221e752512def1a0b4fd
전우선 <jeonwooseon@naver.com>
2025-11-06 21:17:59 +0900

    chore(config): 프로젝트 초기 세팅

---
+++ src/main/kotlin/com/woowacourse/wordcapsule/WordCapsuleApplication.kt
@@ -0,0 +1,11 @@
+package com.woowacourse.wordcapsule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WordCapsuleApplication
+
+fun main(args: Array<String>) {
+	runApplication<WordCapsuleApplication>(*args)
+}
---
+++ src/test/kotlin/com/woowacourse/wordcapsule/WordCapsuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.woowacourse.wordcapsule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WordCapsuleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
