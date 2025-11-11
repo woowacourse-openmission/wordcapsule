bbffed4dec26cf2ba9a42a0d90b438581a3919fb
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-11 20:16:42 +0900

    fix(quiz): 퀴즈 기록 API 테스트 후 수정

--- src/main/kotlin/com/woowacourse/wordcapsule/dto/quiz/QuizRecordRequest.kt
+++ src/main/kotlin/com/woowacourse/wordcapsule/dto/quiz/QuizRecordRequest.kt
@@ -1,15 +1,18 @@
 package com.woowacourse.wordcapsule.dto.quiz
 
-import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 
 data class QuizRecordRequest(
-    @field:NotBlank(message = "생성한 유저 ID는 필수입니다")
+    @field:NotNull(message = "생성한 유저 ID는 필수입니다")
     val userId: Long,
 
     @field:NotNull(message = "퀴즈 설정값 ID는 필수입니다")
     val configId: Long,
 
-    @field:NotBlank(message = "각 퀴즈엔 점수가 필요합니디")
+    @field:NotNull(message = "각 퀴즈엔 점수가 필요합니디")
+    @field:Min(0)
+    @field:Max(10)
     val score: Int,
 )
--- src/main/kotlin/com/woowacourse/wordcapsule/service/quiz/QuizRecordService.kt
+++ src/main/kotlin/com/woowacourse/wordcapsule/service/quiz/QuizRecordService.kt
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 class QuizRecordService(
     private val quizRecordRepository: QuizRecordRepository,
     private val quizConfigRepository: QuizConfigRepository,
