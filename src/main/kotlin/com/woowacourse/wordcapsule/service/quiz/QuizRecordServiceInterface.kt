326f9768eea66793e2e7bccb3bb89afe03107c47
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-12 21:59:51 +0900

    feat(quiz): 퀴즈 기록 목록 조회 API 구현

--- src/main/kotlin/com/woowacourse/wordcapsule/service/quiz/QuizRecordServiceInterface.kt
+++ src/main/kotlin/com/woowacourse/wordcapsule/service/quiz/QuizRecordServiceInterface.kt
@@ -1,7 +1,12 @@
 package com.woowacourse.wordcapsule.service.quiz
 
+import com.woowacourse.wordcapsule.dto.common.PageResponse
+import com.woowacourse.wordcapsule.dto.quiz.QuizRecordListResponse
 import com.woowacourse.wordcapsule.dto.quiz.QuizRecordRequest
+import org.springframework.data.domain.Pageable
 
 interface QuizRecordServiceInterface {
     fun createQuizRecord(request: QuizRecordRequest): Long
+
+    fun getUserQuizRecordList(userId: Long?, pageable: Pageable): PageResponse<QuizRecordListResponse>
 }
