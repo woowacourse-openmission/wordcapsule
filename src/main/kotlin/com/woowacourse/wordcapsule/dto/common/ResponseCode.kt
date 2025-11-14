3948fcc74fd3dac429ad05ed095c87e0b52d924e
Huiyeongkim <huiyeong9619@naver.com>
2025-11-14 18:57:18 +0900

    feat(user): 유저 리스트 조회 API 구현

--- src/main/kotlin/com/woowacourse/wordcapsule/dto/common/ResponseCode.kt
+++ src/main/kotlin/com/woowacourse/wordcapsule/dto/common/ResponseCode.kt
@@ -21,6 +21,7 @@ enum class ResponseCode(
     VALIDATION_FAILED("C001", HttpStatus.BAD_REQUEST, "요청 데이터 검증에 실패했습니다"),
     NOT_FOUND("C002", HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다"),
     METHOD_NOT_ALLOWED("C003", HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP Method입니다"),
+    FORBIDDEN("C004", HttpStatus.FORBIDDEN, "접근 권한이 없습니다"),
     
     // Server Error
     INTERNAL_ERROR("S500", HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다"),
--- src/main/kotlin/com/woowacourse/wordcapsule/service/user/UserServiceInterface.kt
+++ src/main/kotlin/com/woowacourse/wordcapsule/service/user/UserServiceInterface.kt
@@ -1,7 +1,9 @@
 package com.woowacourse.wordcapsule.service.user
 
+import com.woowacourse.wordcapsule.dto.common.PageResponse
 import com.woowacourse.wordcapsule.dto.user.UserCreateRequest
 import com.woowacourse.wordcapsule.dto.user.UserResponse
+import org.springframework.data.domain.Pageable
 
 /**
  * 사용자 관리를 위한 서비스 인터페이스
@@ -16,6 +18,14 @@ interface UserServiceInterface {
      * @return 생성된 사용자 정보
      */
     fun createUser(request: UserCreateRequest): UserResponse
+
+    /**
+     * 사용자 목록을 페이징하여 조회
+     *
+     * @param pageable 페이징 정보
+     * @return 페이징된 사용자 목록
+     */
+    fun getUsers(currentUserId: Long, pageable: Pageable): PageResponse<UserResponse>
 
     /**
      * ID로 사용자 정보 조회
